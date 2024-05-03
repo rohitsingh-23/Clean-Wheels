7e5dfee2480f57567694c66d32936eef1d7d9e1c
Rohit Singh <rohitsingh@Rohits-MacBook-Air-2.local>
2024-05-03 17:42:26 +0530

    first change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        color: "white",
+        fontSize: "15vw",
+        background: "rgb(2,0,36)",
+        background:
+          "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,121,80,1) 74%, rgba(0,212,255,1) 100%)",
+      }}
+    >
+      Clean Wheels
     </div>
   );
 }
